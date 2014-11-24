1c8edc4f0891c7429ec2d182579addaaa278b8ba
dclayto1 <dclayto1@umbc.edu>
2014-11-23 23:03:48 -0500

    Binge Watching App
    Started and finished over this weekend;

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BingeWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.singleVideoMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.entireSeasonMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.entireSeriesMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonRW = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonFF = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            //this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(624, 442);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.ClickEvent += new AxWMPLib._WMPOCXEvents_ClickEventHandler(this.axWindowsMediaPlayer1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.singleVideoMenu,
+            this.entireSeasonMenu,
+            this.entireSeriesMenu});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.fileToolStripMenuItem.Text = "Select";
+            // 
+            // singleVideoMenu
+            // 
+            this.singleVideoMenu.Name = "singleVideoMenu";
+            this.singleVideoMenu.Size = new System.Drawing.Size(144, 22);
+            this.singleVideoMenu.Text = "Single Video";
+            this.singleVideoMenu.Click += new System.EventHandler(this.singleVideoMenu_Click);
+            // 
+            // entireSeasonMenu
+            // 
+            this.entireSeasonMenu.Name = "entireSeasonMenu";
+            this.entireSeasonMenu.Size = new System.Drawing.Size(144, 22);
+            this.entireSeasonMenu.Text = "Entire Season";
+            this.entireSeasonMenu.Click += new System.EventHandler(this.entireSeasonMenu_Click);
+            // 
+            // entireSeriesMenu
+            // 
+            this.entireSeriesMenu.Name = "entireSeriesMenu";
+            this.entireSeriesMenu.Size = new System.Drawing.Size(144, 22);
+            this.entireSeriesMenu.Text = "Entire Series";
+            this.entireSeriesMenu.Click += new System.EventHandler(this.entireSeriesMenu_Click);
+            // 
+            // buttonRW
+            // 
+            this.buttonRW.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.buttonRW.Enabled = false;
+            this.buttonRW.Location = new System.Drawing.Point(75, 0);
+            this.buttonRW.Name = "buttonRW";
+            this.buttonRW.Size = new System.Drawing.Size(75, 33);
+            this.buttonRW.TabIndex = 4;
+            this.buttonRW.Text = "<<";
+            this.buttonRW.UseVisualStyleBackColor = true;
+            this.buttonRW.Click += new System.EventHandler(this.buttonRW_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.buttonPlay.Enabled = false;
+            this.buttonPlay.Location = new System.Drawing.Point(150, 0);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 33);
+            this.buttonPlay.TabIndex = 5;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.buttonPause.Enabled = false;
+            this.buttonPause.Location = new System.Drawing.Point(225, 0);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 33);
+            this.buttonPause.TabIndex = 6;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonFF
+            // 
+            this.buttonFF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.buttonFF.Enabled = false;
+            this.buttonFF.Location = new System.Drawing.Point(300, 0);
+            this.buttonFF.Name = "buttonFF";
+            this.buttonFF.Size = new System.Drawing.Size(75, 33);
+            this.buttonFF.TabIndex = 7;
+            this.buttonFF.Text = ">>";
+            this.buttonFF.UseVisualStyleBackColor = true;
+            this.buttonFF.Click += new System.EventHandler(this.buttonFF_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.buttonNext.Enabled = false;
+            this.buttonNext.Location = new System.Drawing.Point(375, 0);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 33);
+            this.buttonNext.TabIndex = 8;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.buttonPrev.Enabled = false;
+            this.buttonPrev.Location = new System.Drawing.Point(0, 0);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(75, 33);
+            this.buttonPrev.TabIndex = 9;
+            this.buttonPrev.Text = "Prev";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.buttonNext);
+            this.panel1.Controls.Add(this.buttonFF);
+            this.panel1.Controls.Add(this.buttonPause);
+            this.panel1.Controls.Add(this.buttonPlay);
+            this.panel1.Controls.Add(this.buttonRW);
+            this.panel1.Controls.Add(this.buttonPrev);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 409);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(624, 33);
+            this.panel1.TabIndex = 10;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.trackBar1.Location = new System.Drawing.Point(450, 0);
+            this.trackBar1.Maximum = 25;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(167, 45);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.Value = 25;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "Form1";
+            this.Text = "BingeWatcher";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem singleVideoMenu;
+        private System.Windows.Forms.ToolStripMenuItem entireSeasonMenu;
+        private System.Windows.Forms.ToolStripMenuItem entireSeriesMenu;
+        private System.Windows.Forms.Button buttonRW;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonFF;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BingeWatcher
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            axWindowsMediaPlayer1.uiMode = "none";
+            axWindowsMediaPlayer1.enableContextMenu = false;
+            axWindowsMediaPlayer1.Ctlenabled = false;
+            axWindowsMediaPlayer1.stretchToFit = true;
+        }
+
+        private void axWindowsMediaPlayer1_Click(object sender, AxWMPLib._WMPOCXEvents_ClickEvent e)
+        {
+            if (e.nButton == 2)
+            {
+                if (menuStrip1.Visible == true)
+                {
+                    menuStrip1.Visible = false;
+                    panel1.Visible = false;
+                }
+                else
+                {
+                    menuStrip1.Visible = true;
+                    panel1.Visible = true;
+                }
+            }
+        }
+
+        //Play a single video file
+        private void singleVideoMenu_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Title = "Open a single video";
+            openFile.Filter += "Standard Video Files|*.asf;*.wma;*.wmv;*.wm;*.avi;*.mp*;*.m4*;*.wav;*.mov;*.mi*";
+            openFile.Filter += "|All Files|*.*";
+
+            try
+            {
+                if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    axWindowsMediaPlayer1.URL = openFile.FileName;
+                    buttonEnable();
+                    buttonPlay.Enabled = false;
+                    buttonNext.Enabled = false;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //Select a Season folder to play the entire season
+        private void entireSeasonMenu_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFolder = new FolderBrowserDialog();
+            openFolder.Description = "Open individual season folder";
+            openFolder.ShowNewFolderButton = false;
+
+            try
+            {
+                if (openFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    List<string> extensions = new List<string> { "*.asf", "*.wma", "*.wmv", "*.wm", "*.avi", "*.mp*", "*.m4*", "*.wav", "*.mov", "*.mi*" };
+                    List<string> videos = new List<string>(Directory.GetFiles(openFolder.SelectedPath).Where(s =>
+                        s.Contains(".asf") ||
+                        s.Contains(".wma") ||
+                        s.Contains(".wmv") ||
+                        s.Contains(".wm") ||
+                        s.Contains(".avi") ||
+                        s.Contains(".mp") ||
+                        s.Contains(".m4") ||
+                        s.Contains(".wav") ||
+                        s.Contains(".mov") ||
+                        s.Contains(".mi")));
+                    string playlistName = openFolder.SelectedPath;
+                    WMPLib.IWMPPlaylist playlist;
+                    playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist(playlistName);
+
+                    bool isLeftOff = false;
+                    bool fileExists = false;
+                    string episode = "";
+                    double startPos = 0.0;
+                    if (File.Exists(playlistName + @"\bingewatch.txt"))
+                    {
+                        fileExists = true;
+                        string[] lines = System.IO.File.ReadAllLines(playlistName + @"\bingewatch.txt");
+                        episode = lines[0];
+                        startPos = Convert.ToDouble(lines[1]);
+                    }
+
+                    foreach (string eachVideo in videos)
+                    {
+                        if (fileExists == true)
+                        {
+                            if (isLeftOff == true || episode == eachVideo)
+                            {
+                                isLeftOff = true;
+                                WMPLib.IWMPMedia video = axWindowsMediaPlayer1.newMedia(eachVideo);
+                                playlist.appendItem(video);
+                            }
+                        }
+                        else
+                        {
+                            WMPLib.IWMPMedia video = axWindowsMediaPlayer1.newMedia(eachVideo);
+                            playlist.appendItem(video);
+                        }
+                    }
+
+                    axWindowsMediaPlayer1.currentPlaylist = playlist;
+                    axWindowsMediaPlayer1.Ctlcontrols.currentPosition = startPos;
+                    buttonEnable();
+                    buttonPlay.Enabled = false;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //Select a Series folder to play the entire series
+        private void entireSeriesMenu_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFolder = new FolderBrowserDialog();
+            openFolder.Description = "Open series folder";
+            openFolder.ShowNewFolderButton = false;
+
+            try
+            {
+                if (openFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string[] seasons = Directory.GetDirectories(openFolder.SelectedPath);
+                    string playlistName = openFolder.SelectedPath;
+                    WMPLib.IWMPPlaylist playlist;
+                    playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist(playlistName);
+                    double startPos = 0.0;
+
+                    foreach (string eachSeason in seasons)
+                    {
+                        List<string> extensions = new List<string> { "*.asf", "*.wma", "*.wmv", "*.wm", "*.avi", "*.mp*", "*.m4*", "*.wav", "*.mov", "*.mi*" };
+                        List<string> videos = new List<string>(Directory.GetFiles(eachSeason).Where(s =>
+                            s.Contains(".asf") ||
+                            s.Contains(".wma") ||
+                            s.Contains(".wmv") ||
+                            s.Contains(".wm") ||
+                            s.Contains(".avi") ||
+                            s.Contains(".mp") ||
+                            s.Contains(".m4") ||
+                            s.Contains(".wav") ||
+                            s.Contains(".mov") ||
+                            s.Contains(".mi")));
+
+                        Console.WriteLine(playlistName);
+                        bool isLeftOff = false;
+                        bool fileExists = false;
+                        string episode = "";
+                        if (File.Exists(playlistName + @"\bingewatch.txt"))
+                        {
+                            fileExists = true;
+                            string[] lines = System.IO.File.ReadAllLines(playlistName + @"\bingewatch.txt");
+                            episode = lines[0];
+                            startPos = Convert.ToDouble(lines[1]);
+                        }
+
+                        foreach (string eachVideo in videos)
+                        {
+                            if (fileExists == true)
+                            {
+                                if (isLeftOff == true || episode == eachVideo)
+                                {
+                                    isLeftOff = true;
+                                    WMPLib.IWMPMedia video = axWindowsMediaPlayer1.newMedia(eachVideo);
+                                    playlist.appendItem(video);
+                                }
+                            }
+                            else
+                            {
+                                WMPLib.IWMPMedia video = axWindowsMediaPlayer1.newMedia(eachVideo);
+                                playlist.appendItem(video);
+                            }
+                        }
+                    }
+
+                    axWindowsMediaPlayer1.currentPlaylist = playlist;
+                    axWindowsMediaPlayer1.Ctlcontrols.currentPosition = startPos;
+                    buttonEnable();
+                    buttonPlay.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentMedia.sourceURL != null)
+            {
+                buttonPrev.Enabled = true;
+                buttonPlay.Enabled = false;
+                buttonPause.Enabled = true;
+                buttonRW.Enabled = true;
+                buttonFF.Enabled = true;
+
+                writeCurrentPos();
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                axWindowsMediaPlayer1.settings.rate = 1.0;
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentMedia.sourceURL != null)
+            {
+                buttonPrev.Enabled = true;
+                buttonPlay.Enabled = true;
+                buttonPause.Enabled = false;
+                buttonRW.Enabled = true;
+                buttonFF.Enabled = true;
+
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                writeCurrentPos();
+            }
+        }
+
+        private void buttonFF_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentMedia.sourceURL != null)
+            {
+                buttonPrev.Enabled = true;
+                buttonPlay.Enabled = true;
+                buttonPause.Enabled = true;
+                buttonRW.Enabled = true;
+                buttonFF.Enabled = false;
+
+                axWindowsMediaPlayer1.settings.rate = 2.0;
+                writeCurrentPos();
+            }
+        }
+
+        private void buttonRW_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentMedia.sourceURL != null)
+            {
+                buttonPrev.Enabled = true;
+                buttonPlay.Enabled = true;
+                buttonPause.Enabled = true;
+                buttonRW.Enabled = false;
+                buttonFF.Enabled = true;
+
+                axWindowsMediaPlayer1.settings.rate = -0.5;
+                writeCurrentPos();
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentMedia.sourceURL != null)
+            {
+                if (axWindowsMediaPlayer1.currentMedia.sourceURL == axWindowsMediaPlayer1.currentPlaylist.get_Item(axWindowsMediaPlayer1.currentPlaylist.count - 2).sourceURL)
+                {
+                    buttonNext.Enabled = false;
+                }
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                axWindowsMediaPlayer1.Ctlcontrols.next();
+                axWindowsMediaPlayer1.settings.rate = 1.0;
+                buttonPrev.Enabled = true;
+                buttonPlay.Enabled = false;
+                buttonPause.Enabled = true;
+                buttonRW.Enabled = true;
+                buttonFF.Enabled = true;
+
+                writeCurrentPos();
+            }
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.currentMedia.sourceURL != null)
+            {
+                if (axWindowsMediaPlayer1.Ctlcontrols.currentPosition > 3)
+                {
+                    axWindowsMediaPlayer1.Ctlcontrols.currentPosition = 0;
+                }
+                else
+                {
+                    if (axWindowsMediaPlayer1.currentMedia.sourceURL == axWindowsMediaPlayer1.currentPlaylist.get_Item(0).sourceURL)
+                    {
+                        axWindowsMediaPlayer1.Ctlcontrols.currentPosition = 0;
+                    }
+                    else
+                    {
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                        axWindowsMediaPlayer1.Ctlcontrols.previous();
+                        buttonNext.Enabled = true;
+                    }
+                }
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                axWindowsMediaPlayer1.settings.rate = 1.0;
+                buttonPrev.Enabled = true;
+                buttonPlay.Enabled = false;
+                buttonPause.Enabled = true;
+                buttonRW.Enabled = true;
+                buttonFF.Enabled = true;
+
+                writeCurrentPos();
+            }
+        }
+
+        private void buttonEnable()
+        {
+            axWindowsMediaPlayer1.settings.rate = 1.0;
+            buttonPrev.Enabled = true;
+            buttonPlay.Enabled = true;
+            buttonPause.Enabled = true;
+            buttonRW.Enabled = true;
+            buttonNext.Enabled = true;
+            buttonFF.Enabled = true;
+        }
+
+        private void writeCurrentPos()
+        {
+            if (axWindowsMediaPlayer1.currentPlaylist.name != "Playlist1")
+            {
+                string file = axWindowsMediaPlayer1.currentPlaylist.name + @"\bingewatch.txt";
+                File.Delete(file);
+                StreamWriter outfile = new StreamWriter(file);
+                outfile.WriteLine(axWindowsMediaPlayer1.currentMedia.sourceURL);
+                outfile.WriteLine(axWindowsMediaPlayer1.Ctlcontrols.currentPosition);
+                outfile.Close();
+                File.SetAttributes(file, File.GetAttributes(file) | FileAttributes.Normal | FileAttributes.Hidden);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume = trackBar1.Value;
+        }
+    }
+}
